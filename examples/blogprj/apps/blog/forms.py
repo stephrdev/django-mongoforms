7d4d44d0dc0f5768fe0263f7c5f48a959dbf381e
Florian Schlachter <flori@n-schlachter.de>
2010-05-01 22:37:15 +0800

    some blog improvements

--- examples/blogprj/apps/blog/forms.py
+++ examples/blogprj/apps/blog/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django import forms
 from mongoforms import MongoForm
 from models import BlogPost
 
@@ -7,3 +8,4 @@ class BlogPostForm(MongoForm):
     class Meta:
         document = BlogPost
         fields = ('author', 'title', 'content', 'published')
+    content = forms.CharField(widget=forms.Textarea)
