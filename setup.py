3f52896b5e3b1f06724b7eba298c2eebbeac3c18
Stephan Jaekel <steph@rdev.info>
2010-05-01 03:20:46 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='django-mongoforms',
+    version='0.1',
+    description='A Django-ModelForm clone for mongoengine',
+    author='Stephan Jaekel',
+    author_email='steph@rdev.info',
+    url='http://github.com/stephrdev/django-mongoforms/',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe=False,
+)
