c8af5928d2dd84fbc0e3e0163c93e0a54ce9bdea
Serge Matveenko <s@matveenko.ru>
2012-05-21 16:24:16 +0400

    Organize tests.

--- testprj/testapp/views.py
+++ testprj/testapp/views.py
@@ -5,14 +5,14 @@ from forms import Test001ChildForm
 
 @render_test
 def test001(request):
-    
+
     if request.method == 'POST':
         form = Test001ChildForm(request.POST)
-        
+
         if form.is_valid():
             form.save()
-        
+
     else:
         form = Test001ChildForm()
-    
+
     return {'form': form}
--- testprj/tests.py
+++ testprj/tests.py
@@ -1,27 +1,25 @@
-import os, sys
-
 from django.test.simple import DjangoTestSuiteRunner
 from django.test.testcases import TestCase
 
 
 class MongoengineDjangoTestSuiteRunner(DjangoTestSuiteRunner):
-    
+
     def setup_databases(self, **kwargs):
         return None
-    
+
     def teardown_databases(self, old_config, **kwargs):
         pass
 
 
 class MongoengineTestCase(TestCase):
     """ completely dummy test case class """
-    
+
     def setUp(self):
         TestCase.setUp(self)
-    
+
     def _fixture_setup(self):
         pass
-    
+
     def _fixture_teardown(self):
         pass
 
